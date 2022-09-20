734daaf2a8eead0e75a641d6b58228996ead5986
anishazahan <anishazahan13@gmail.com>
2022-09-20 22:38:24 +0600

    menu add

--- src/Shared/Header/Navbar.js
+++ src/Shared/Header/Navbar.js
@@ -1,15 +1,35 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import logoImg from '../../assets/logo.png'
+import {FaUserAlt} from 'react-icons/fa'
+import {BsCartFill} from 'react-icons/bs'
 
 const Navbar = () => {
   return (
     <div className='max-w-7xl mx-auto lg:px-5 px-6 md:px-10'>
-            <div className="flex">
+            <div className="flex justify-between items-center">
                 <div className="logo">
                     <img className='w-40' src={logoImg} alt="" />
                 </div>
-                <div className="menu"></div>
-                <div className="cart-icons"></div>
+                <div className="menu">
+                    <div className='flex items-center space-x-5'>
+                        <NavLink to='/' className='active font-medium'>Home</NavLink>
+                        <NavLink to='about' className='active font-medium'>About</NavLink>
+                        <NavLink to='products' className='active font-medium'>Products</NavLink>
+                        <NavLink to='/' className='active font-medium'>Privacy Policy</NavLink>
+                        <NavLink to='/' className='active font-medium'>Shiping & returns</NavLink>
+                        <NavLink to='/' className='active font-medium'>Blog</NavLink>
+                        <NavLink to='/' className='active font-medium'>Contact</NavLink>
+                        <NavLink to='/' className='active font-medium'>other</NavLink>
+                       
+                        </div>  
+                </div>
+                <div className="cart-icons flex items-center space-x-3">
+                    <p>Login</p>
+                    <p><FaUserAlt></FaUserAlt> </p>
+                    <p><BsCartFill></BsCartFill> </p>
+                    <p>Admin</p>
+                </div>
             </div>
 
     </div>
