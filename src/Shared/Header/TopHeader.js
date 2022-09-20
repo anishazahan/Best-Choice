5146cd9cfc67875adc24d4b9ac47d46e8f7b2134
anishazahan <anishazahan13@gmail.com>
2022-09-20 21:49:17 +0600

    top header section design

--- src/Shared/Header/TopHeader.js
+++ src/Shared/Header/TopHeader.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import './Header.css'
-import {BsTelephoneFill} from 'react-icons/bs'
+import {BsTelephoneFill,BsFillEnvelopeFill} from 'react-icons/bs'
+import { Link } from 'react-router-dom'
 
 const TopHeader = () => {
   return (
     <div className='top-header'>
-        <div className="flex h-full max-w-7xl px-16">
-            <div className="left">
-                <div className="flex items-center text-white h-full space-x-2">
+        <div className="flex h-full max-w-7xl px-16 justify-between">
+            <div className="left flex space-x-5 items-center">
+                <div className="flex items-center text-white h-full space-x-1">
                     <p><BsTelephoneFill></BsTelephoneFill> </p>
                     <p className='text-sm'>+287675546464</p>
+                </div>
+                <div className="flex items-center text-white h-full space-x-1">
+                    <p><BsFillEnvelopeFill></BsFillEnvelopeFill> </p>
+                    <p className='text-sm'>contact@gmail.com</p>
+                </div>
+            </div>
+            <div className="right flex space-x-5 items-center">
+                <div className="flex items-center text-white h-full space-x-1">
+                    
+                    <Link to='/' className='text-sm link'>FAQS</Link>
+                </div>
+                <div className="flex items-center text-white h-full space-x-1">
+                <Link to='/' className='text-sm link'>Need Help?</Link>
                 </div>
             </div>
         </div>
