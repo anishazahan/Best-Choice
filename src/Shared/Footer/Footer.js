a97cdf5936a1da3820f6a6ed3b846f668a46d06b
anishazahan <anishazahan13@gmail.com>
2022-09-21 08:33:28 +0600

    top footer design

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -1,38 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Footer.css'
+
 
 const Footer = () => {
   return (
-    <div>
+    <div className="footer-section">
     
-    <footer className="footer p-10 bg-base-200 text-base-content">
+    <footer className=" px-5 md:px-7 lg:px-10 py-16 text-white mx-auto flex flex-col md:flex-row max-w-7xl justify-between">
   <div>
-    <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
-    <p>ACME Industries Ltd.<br/> Providing reliable tech since 1992</p>
+    <h1 className="mb-5 text-2xl font-bold footer-title">Best Choice</h1>
+    <p className="font-medium">Best Choice Smart Service Company<br/> Providing reliable tech since   {(new Date().getFullYear())}</p>
   </div> 
-  <div>
-    <span className="footer-title">Services</span> 
-    <Link to='/' className="link link-hover">Branding</Link> 
-    <Link to='/' className="link link-hover">Design</Link> 
-    <Link to='/' className="link link-hover">Marketing</Link> 
+ <div className="flex flex-col md:flex-row justify-between space-x-36">
+ <div>
+    <h1 className=" mb-5 footer-title">Services</h1> 
+    <div className="flex flex-col space-y-2">
+    <Link to='/' className="link link-hover">Product</Link> 
+    <Link to='/' className="link link-hover">Available</Link> 
+    <Link to='/' className="link link-hover">Blog</Link> 
     <Link to='/' className="link link-hover">Advertisement</Link>
+    </div>
   </div> 
   <div>
-    <span className="footer-title">Company</span> 
-    <Link to='/' className="link link-hover">About us</Link> 
+    <h1 className="footer-title mb-5">Company</h1> 
+  <div className="flex flex-col space-y-2">
+  <Link to='/' className="link link-hover">About us</Link> 
     <Link to='/' className="link link-hover">Contact</Link> 
-    <Link to='/' className="link link-hover">Jobs</Link> 
-    <Link to='/' className="link link-hover">Press kit</Link>
+    <Link to='/' className="link link-hover">Privacy Policy</Link> 
+    <Link to='/' className="link link-hover">Shiping & Return</Link>
+  </div>
   </div> 
+ </div>
   <div>
-    <span className="footer-title">Newsletter</span> 
+    <h1 className="footer-title mb-5">Newsletter</h1> 
     <div className="form-control w-80">
       <label className="label">
-        <span className="label-text">Enter your email address</span>
+        <span className="label-text text-white">Enter your email address</span>
       </label> 
       <div className="relative">
-        <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" /> 
-        <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
+        <input type="text" placeholder="Your Email" className="input input-bordered w-full pr-16" /> 
+        <button className="btn btn-secondary absolute top-0 right-0 rounded-l-none">Subscribe</button>
       </div>
     </div>
   </div>
