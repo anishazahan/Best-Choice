1084449b85378cf6013a87d0ff9368d9499fd84f
anishazahan <anishazahan13@gmail.com>
2022-09-21 11:55:41 +0600

    fetch blog data for homeBlog

--- src/Pages/Blog/HomeBolg.js
+++ src/Pages/Blog/HomeBolg.js
@@ -15,6 +15,7 @@ const HomeBolg = () => {
     
   return (
     <div className='max-w-7xl mx-auto'>
+        <h1 className='text-2xl font-semibold'>Our latest Blog</h1>
         <div className="grid grid-cols-2 gap-3 m-0 p-0">
             {
                 blogs && blogs.map(blog=>{
@@ -25,13 +26,17 @@ const HomeBolg = () => {
                         <h2 className='mt-4 mb-2 font-bold'>{blog.tittle}</h2>
                         <h2 className='mb-3 font-medium text-gray-600 text-sm'>Posted By Admin 22 Apr {(new Date().getFullYear())}</h2>
                         <p className='text-sm pr-10'>{(blog.description).slice(0,149)}</p>
-                        <div className="">
-                            <button className=' py-1 hover:border hover:bg-transparent transition-all hover:border-1 bg-secondary text-white border-secondary roinded-sm hover:text-secondary font-medium px-10'>Details</button>
+                        <div className="mt-5">
+                            <button className=' py-2 overflow-hidden hover:border hover:bg-transparent transition-all hover:border-1 bg-secondary text-white border-secondary roinded-sm hover:text-secondary font-medium px-10'>Details</button>
                         </div>
                     </div>
                 }).slice(0,2)
             }
+           
         </div>
+        <div className="text-right mb-16 mt-10 ">
+                <button className='text-secondary link font-medium text-sm'>See More</button>
+            </div>
 
     </div>
   )
