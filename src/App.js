76956c84f99258c837f2ff0cfc757f5e193174db
anishazahan <anishazahan13@gmail.com>
2022-09-21 08:04:45 +0600

    footer section add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Shared/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Footer from './Shared/Footer/Footer';
+
+
+
 
 function App() {
   return (
-    <div className="">
+    <div>
        <Header></Header>
+       <Routes>
+       <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="about" element={<About />} />
+       </Routes>
+       <Footer></Footer>
     </div>
   );
 }
