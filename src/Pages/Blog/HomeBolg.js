45e1f4cfb0ad64a7e3041b6e20b2ff1564e2fad8
anishazahan <anishazahan13@gmail.com>
2022-09-21 11:49:16 +0600

    home blog add

--- src/Pages/Blog/HomeBolg.js
+++ src/Pages/Blog/HomeBolg.js
@@ -22,8 +22,12 @@ const HomeBolg = () => {
                         <div className="">
                             <img className='w-full' src={blog.img} alt="" />
                         </div>
-                        <h2 className='mt-4 mb-3 font-bold'>{blog.tittle}</h2>
-                        <h2 className=' mb-3 font-medium text-gray-400'>Posted By Admin 22 Apr {(new Date().getFullYear())}</h2>
+                        <h2 className='mt-4 mb-2 font-bold'>{blog.tittle}</h2>
+                        <h2 className='mb-3 font-medium text-gray-600 text-sm'>Posted By Admin 22 Apr {(new Date().getFullYear())}</h2>
+                        <p className='text-sm pr-10'>{(blog.description).slice(0,149)}</p>
+                        <div className="">
+                            <button className=' py-1 hover:border hover:bg-transparent transition-all hover:border-1 bg-secondary text-white border-secondary roinded-sm hover:text-secondary font-medium px-10'>Details</button>
+                        </div>
                     </div>
                 }).slice(0,2)
             }
