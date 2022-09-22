ff6a0c63830b98080f85e209a220c42bd56806d1
anishazahan <anishazahan13@gmail.com>
2022-09-22 17:56:43 +0600

    sponcher sections design

---
+++ src/Pages/Home/Componants/Sponcer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import img1 from '../../../assets/brands-1.jpg'
+import img2 from '../../../assets/brands-2.jpg'
+import img3 from '../../../assets/brands-3.jpg'
+import img4 from '../../../assets/brands-4.jpg'
+import img5 from '../../../assets/brands-5.jpg'
+
+const Sponcer = () => {
+  return (
+    <>
+        <div className="max-w-7xl mx-auto">
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5">
+
+            </div>
+        </div>
+
+    </>
+  )
+}
+
+export default Sponcer
