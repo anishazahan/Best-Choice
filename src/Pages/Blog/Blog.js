f08dbc27a0c1b47465be8377c2840bcc78b12c85
anishazahan <anishazahan13@gmail.com>
2022-09-23 10:57:18 +0600

    blog details page responsive

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -22,7 +22,7 @@ const Blog = () => {
                         <h2 className='mb-3 font-medium text-gray-500 text-sm'>Posted By Admin 22 Apr {(new Date().getFullYear())}</h2>
                         <p className='text-justify mt-5 text-gray-500'>{(blog.description).slice(0,149)}</p>
                         <div className="mt-5">
-                            <button className=' py-2 overflow-hidden hover:border hover:bg-transparent transition-all hover:border-1 bg-primary text-white border-secondary rounded-sm hover:text-secondary font-medium px-10'>Reed More</button>
+                            <button className=' py-2 overflow-hidden  border  transition-all border-1 border-secondary rounded-sm hover:bg-slate-200 text-secondary font-medium px-10'>Reed More</button>
                         </div>
                         <div className="icons flex space-x-5 mt-10">
                             <Link className='text-2xl rounded-full hover:bg-primary text-white p-2  shadow-md bg-secondary' to='/'> <AiFillFacebook></AiFillFacebook> </Link>
