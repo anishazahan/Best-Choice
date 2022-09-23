af2aa72543231250c60a597925a68a4f7df3bc39
anishazahan <anishazahan13@gmail.com>
2022-09-23 06:30:35 +0600

    product details page design

--- src/Pages/Products/ProductsDetails.js
+++ src/Pages/Products/ProductsDetails.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import img from '../../../src/assets/products.jpg'
-import {AiFillStar} from 'react-icons/ai'
+import {AiFillStar,} from 'react-icons/ai'
 import {MdOutlineStarHalf} from 'react-icons/md'
+import {FiChevronDown,FiChevronUp} from 'react-icons/fi'
+import {AiFillFacebook,AiFillLinkedin,AiFillInstagram,AiFillTwitterSquare} from 'react-icons/ai'
+import { Link } from 'react-router-dom'
 
 const ProductsDetails = () => {
+    const [quantity,setQuantity] = useState(0)
   return (
     <>
         <div className="max-w-7xl mx-auto lg:px-10 px-5">
@@ -30,6 +34,25 @@ const ProductsDetails = () => {
                         <p className='text-gray-600 text-sm mt-2'>Weight:1.00 LBS</p>
                         <p className='text-gray-600 text-sm mt-2 mb-4'>Shipping:Calculated at Checkout</p>
                         <hr />
+                        <h2 className='text-[18px] font-semibold mt-3 mb-4'>Price:29.00$</h2>
+                        <div className="flex items-center space-x-3">
+                            <h1>Quantity: </h1>
+                          <div className="flex space-x-4 items-center">
+                          <button className='border p-1 bg-primary text-white' onClick={()=>setQuantity(quantity+1)}> <FiChevronDown></FiChevronDown> </button>
+                            <button className='font-semibold'>1</button>
+                            <button className='border p-1 bg-primary text-white' onClick={()=>setQuantity(quantity-1)}> <FiChevronUp></FiChevronUp> </button>
+                          </div>
+                        </div>
+                        <div className=" mt-10 space-x-3">
+                            <button className='px-10 py-[.57rem] bg-primary text-white text-sm font-medium'>Add To Cart</button>
+                            <button className='px-8 py-2 border border-secondary text-secondary text-sm font-medium'>Add To Wishlist</button>
+                        </div>
+                        <div className="icons flex space-x-5 mt-10">
+                            <Link className='text-3xl rounded-sm hover:bg-primary hover:text-white text-secondary p-2  shadow-lg' to='/'> <AiFillFacebook></AiFillFacebook> </Link>
+                            <Link className='text-3xl rounded-sm hover:bg-primary hover:text-white text-secondary p-2  shadow-lg' to='/'> <AiFillLinkedin></AiFillLinkedin> </Link>
+                            <Link className='text-3xl rounded-sm hover:bg-primary hover:text-white text-secondary p-2  shadow-lg' to='/'> <AiFillInstagram></AiFillInstagram> </Link>
+                            <Link className='text-3xl rounded-sm hover:bg-primary hover:text-white text-secondary p-2  shadow-lg' to='/'> <AiFillTwitterSquare></AiFillTwitterSquare> </Link>
+                        </div>
                     </div>
                 </div>
             </div>
