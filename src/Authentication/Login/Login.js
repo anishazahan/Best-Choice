0ed9a6251aa29c27ecc7790bb1b95a6d5d137c83
anishazahan <anishazahan13@gmail.com>
2022-09-24 19:22:45 +0600

    email form valid

--- src/Authentication/Login/Login.js
+++ src/Authentication/Login/Login.js
@@ -3,21 +3,101 @@ import auth from '../../firebase.init';
 import './Login.css'
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import googleIcon from '../../assets/googleIcon.png'
+import { useForm } from "react-hook-form";
 
 const Login = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+      } = useForm();
+      
+    const onSubmit = data =>{
+        if(errors){
+            
+            console.log(errors)
+        }
+      
+    };
+    
     
   return (
     <>
         <div className="login-background relative flex justify-center">
-                <div className="login-box bg-white absolute shadow-md pt-10 mt-10 pl-7 pr-10 pb-14">
+                <div className="login-box bg-white absolute shadow-md pt-10 mt-10 pl-7 pr-10 pb-14 w-[35%]">
                     <h2 className='text-2xl font-semibold mb-7'>Please Login!!</h2>
                     <div onClick={() => signInWithGoogle()} className="border flex">
                     <img className='w-10' src={googleIcon} alt="" />
                     <button className='py-2 pr-10 pl-5 font-medium'> Continue With Google</button>
                     </div>
                     <div className="divider">OR</div>
-                </div>
+
+                    <form className='w-full' onSubmit={handleSubmit(onSubmit)}>
+     
+     <input
+       type="email"
+       className=" pl-3 py-[10px] placeholder-black form-control w-full bg-transparent border border-2 outline-none text-sm text-black font-medium mb-2 focus:border-secondary"
+       placeholder="Your Email"
+       {...register("email", {
+         required: {
+           value: true,
+           message: "Email must be Required",
+         },
+         pattern: {
+           value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+           message: "Enter Valid Email",
+         },
+       })}
+     />
+     <div className="mb-2">
+       {errors.email?.type === "required" && (
+         <p className="text-xs text-red-600 mb-2 ">
+           {errors.email.message}
+         </p>
+       )}
+       {errors.email?.type === "pattern" && (
+         <p className="text-xs text-red-600 mb-2 ">
+           {errors.email.message}
+         </p>
+       )}
+     </div>
+   
+
+  
+     <input
+       type="password"
+       className="form form-control py-2 pl-3 mt-3 "
+       placeholder="Your Password"
+       {...register("password", {
+         required: {
+           value: true,
+           message: "Password must be Required",
+         },
+         minLength: {
+           value: 6,
+           message: "Password must be min six character",
+         },
+       })}
+     />
+
+     <div className=" mb-2 ">
+       {errors.password?.type === "required" && (
+         <p className="font-medium text-danger mb-3 ">
+           {errors.password.message}
+         </p>
+       )}
+       {errors.password?.type === "minLength" && (
+         <p className="font-medium text-danger">
+           {errors.password.message}
+         </p>
+       )}
+     </div>
+  
+       
+  <input type="submit" value="Login" className="btn btn-info fw-bold px-4 py-2 mt-3 mb-5" />
+ </form>
+        </div>
         </div>
     </>
   )
