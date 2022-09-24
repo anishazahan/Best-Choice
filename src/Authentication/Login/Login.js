eea697d7e2a73bd7a1db71df1f8d900e1b1e2797
anishazahan <anishazahan13@gmail.com>
2022-09-24 17:02:45 +0600

    sign in with google authentication added

--- src/Authentication/Login/Login.js
+++ src/Authentication/Login/Login.js
@@ -2,14 +2,21 @@ import React from 'react'
 import auth from '../../firebase.init';
 import './Login.css'
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import googleIcon from '../../assets/googleIcon.png'
 
 const Login = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    
   return (
     <>
         <div className="login-background relative flex justify-center">
-                <div className="login-box bg-white absolute shadow-md pt-10 mt-10">
-                    <h2 className='text-2xl font-semibold'>Please Login!!</h2>
+                <div className="login-box bg-white absolute shadow-md pt-10 mt-10 pl-7 pr-10 pb-14">
+                    <h2 className='text-2xl font-semibold mb-7'>Please Login!!</h2>
+                    <div onClick={() => signInWithGoogle()} className="border flex">
+                    <img className='w-10' src={googleIcon} alt="" />
+                    <button className='py-2 pr-10 pl-5 font-medium'> Continue With Google</button>
+                    </div>
+                    <div className="divider">OR</div>
                 </div>
         </div>
     </>
