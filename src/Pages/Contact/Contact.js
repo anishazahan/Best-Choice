d03bc4521b1594041118db41a190951b1662e3b7
anishazahan <anishazahan13@gmail.com>
2022-11-25 17:11:00 +0600

    contact section design

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -3,15 +3,12 @@ import React from 'react'
 const Contact = () => {
   return (
     <>
-<div class="flex min-h-screen mt-[4%] justify-start bg-white">
+<div class="flex min-h-screen mt-[4%] justify-start bg-white mx-4 md:mx-0">
   <div class="mx-auto w-full max-w-lg">
     <h1 class="text-4xl font-medium text-primary font-semibold">Contact us_____</h1>
     <p class="mt-3">Email us at contact@help.com or message us here:</p>
 
-    <form action="https://api.web3forms.com/submit" class="mt-10">
-    
-   
-
+    <form class="mt-10">
       <input type="hidden" name="access_key" value="YOUR_ACCESS_KEY_HERE" /> 
       <div class="grid gap-6 sm:grid-cols-2">
         <div class="relative z-0">
@@ -27,7 +24,7 @@ const Contact = () => {
           <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-primary peer-focus:dark:text-blue-500">Your message</label>
         </div>
       </div>
-      <button type="submit" class="mt-5 rounded-md bg-black px-10 py-2 text-white">Send Message</button>
+      <button type="submit" class="mt-5 rounded-md bg-secondary px-10 py-2 text-white">Send Message</button>
     </form>
   </div>
 </div>
