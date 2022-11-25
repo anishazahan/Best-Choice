d46fd146f922b5787798f3eef952387878bb5baa
anishazahan <anishazahan13@gmail.com>
2022-11-25 17:12:50 +0600

    contact section responsive

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
     <>
 <div class="flex min-h-screen mt-[4%] justify-start bg-white mx-4 md:mx-0">
   <div class="mx-auto w-full max-w-lg">
-    <h1 class="text-4xl font-medium text-primary font-semibold">Contact us_____</h1>
+    <h1 class="text-4xl text-primary font-semibold">Contact us_____</h1>
     <p class="mt-3">Email us at contact@help.com or message us here:</p>
 
     <form class="mt-10">
@@ -24,7 +24,7 @@ const Contact = () => {
           <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-primary peer-focus:dark:text-blue-500">Your message</label>
         </div>
       </div>
-      <button type="submit" class="mt-5 rounded-md bg-secondary px-10 py-2 text-white">Send Message</button>
+      <button type="submit" class="mt-5 rounded-sm bg-secondary px-10 py-2 text-white">Send Message</button>
     </form>
   </div>
 </div>
