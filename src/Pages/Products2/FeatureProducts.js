cefe035ee6517a22b6db121d3e27792b0987d864
anishazahan <anishazahan13@gmail.com>
2023-01-15 14:01:39 +0600

    css styling for feature product

--- src/Pages/Products2/FeatureProducts.js
+++ src/Pages/Products2/FeatureProducts.js
@@ -16,9 +16,9 @@ const FeatureProducts = () => {
 
   return (
     <>
-        <div className="max-w-7xl px-10 md:px-20 mx-auto">
+        <div className="max-w-5xl px-10 mx-auto">
             <h2 className='text-sm font-bold'>OUR FEATURE PRODUCTS</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10 mb-40">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10 mb-40">
 
             {
                 FeatureProducts.map((currentElement)=>{
--- src/Pages/Products2/Products.js
+++ src/Pages/Products2/Products.js
@@ -9,7 +9,7 @@ const Products = (currentElement) => {
   return (
     <>
     <NavLink to={`singleProduct/${id}`}>
-      <div className="card shadow-2xl">
+      <div className="card shadow-2xl rounded-none border border-2">
           <div className="relative">
             <img src={image} alt={name} />
               <h2 className='absolute top-5 right-5 px-4 rounded-3xl font-semibold py-2 bg-white'>{name}</h2>
