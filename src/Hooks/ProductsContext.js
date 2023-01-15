c03215f77bc92b8d9b03bea264cc85649d00d166
anishazahan <anishazahan13@gmail.com>
2023-01-15 15:23:56 +0600

    state add for single product

--- src/Hooks/ProductsContext.js
+++ src/Hooks/ProductsContext.js
@@ -1,53 +1,62 @@
-
-
-
-
 import axios from "axios";
 import { useReducer } from "react";
 import { useEffect } from "react";
 import { createContext, useContext } from "react";
-import reducer from './reducer/ProductsReducer'
+import reducer from "./reducer/ProductsReducer";
 
-const AppContext =createContext();
+const AppContext = createContext();
 
-const api = "https://api.pujakaitem.com/api/products"
-const initilaState = {
-    isLoading : false,
-    isError : false,
-    products : [],
-    FeatureProducts : []
-}
+const api = "https://api.pujakaitem.com/api/products";
+const initialState = {
+  isLoading: false,
+  isError: false,
+  products: [],
+  FeatureProducts: [],
+  isSingleLoading: false,
+  singleProduct :{}
+};
 
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
+  const getProducts = async (url) => {
+    dispatch({ type: "SET_LOADING" });
+    try {
+      const res = await axios.get(url);
+      const products = await res.data;
+      // console.log(products);
+      dispatch({ type: "SET_API_DATA", payload: products });
+    } catch (error) {
+      dispatch({ type: "API_ERROR" });
+    }
+  };
 
+  //--      api call for single products
 
-const AppProvider = ({children}) => {
-    const [state,dispatch] = useReducer(reducer,initilaState);
+  const getSingleProduct = async (url) => {
+    dispatch({ type: "SET_SINGLE_LOADING" });
+    try {
+      const res = await axios.get(url);
+      const singleProduct = await res.data;
+      dispatch({ type: "SET_SINGLE_PRODUCT", payload: singleProduct });
+    } catch (error) {
+      dispatch({ type: "SET_SINGLE_ERROR" });
+    }
+  };
 
-    const getProducts = async (url)=>{
-        dispatch({type:"SET_LOADING"});
-        try {
-	    const res = await axios.get(url);
-	        const products = await res.data
-	        // console.log(products);
-	        dispatch({type:"SET_API_DATA", payload:products})
-        } catch (error) {
-            dispatch({type:"API_ERROR"});
-            
-        }
+  useEffect(() => {
+    getProducts(api);
+  }, []);
 
-     }
+  return (
+    <AppContext.Provider value={{ ...state, getSingleProduct }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
 
-    useEffect(()=>{
-        getProducts(api);
-    },[])
+//    custom hook
 
-    return ( <AppContext.Provider value={{...state}}>{children}</AppContext.Provider>)
-}
+const useProductContext = () => useContext(AppContext);
 
-//    custom hook  
-
-const useProductContext = () =>useContext(AppContext);
-
-
-export {AppProvider,AppContext,useProductContext};
+export { AppProvider, AppContext, useProductContext };
