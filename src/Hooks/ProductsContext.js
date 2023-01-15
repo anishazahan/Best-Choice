5361c4aefc8909151b74ba17da841bf20971d422
anishazahan <anishazahan13@gmail.com>
2023-01-15 11:51:47 +0600

    products reducer component create

--- src/Hooks/ProductsContext.js
+++ src/Hooks/ProductsContext.js
@@ -3,14 +3,26 @@
 
 
 import axios from "axios";
+import { useReducer } from "react";
 import { useEffect } from "react";
 import { createContext, useContext } from "react";
+import reducer from './reducer/ProductsReducer'
 
 const AppContext =createContext();
 
 const api = "https://api.pujakaitem.com/api/products"
+const initilaState = {
+    isLoading : false,
+    isError : false,
+    products : [],
+    FeatureProducts : []
+}
+
+
+
 
 const AppProvider = ({children}) => {
+    const [state,dispatch] = useReducer(reducer,initilaState);
 
     const getProducts = async (url)=>{
         const res = await axios.get(url);
@@ -22,9 +34,7 @@ const AppProvider = ({children}) => {
         getProducts(api);
     },[])
 
-
-    return ( <AppContext.Provider value={{name : "anisha"}}>{children}</AppContext.Provider>)
-
+    return ( <AppContext.Provider value={{...state}}>{children}</AppContext.Provider>)
 }
 
 //    custom hook  
---
+++ src/Hooks/reducer/ProductsReducer.js
@@ -0,0 +1,8 @@
+
+
+const ProductReducer = (state,action)=>{
+
+}
+
+
+export default ProductReducer
