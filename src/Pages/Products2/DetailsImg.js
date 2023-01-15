917bae9890da0e71e5dbeed99438b3f7fe23e703
anishazahan <anishazahan13@gmail.com>
2023-01-15 23:48:48 +0600

    click img load dynamicaly

--- src/Pages/Products2/DetailsImg.js
+++ src/Pages/Products2/DetailsImg.js
@@ -1,18 +1,20 @@
 import React from 'react'
+import { useState } from 'react';
 
-const DetailsImg = ({imgs}) => {
+const DetailsImg = ({imgs = [{url :""}]}) => {
     console.log(imgs);
+    const [mailImg,setMainImg] = useState(imgs[0])
   return (
     <>
     
                     <div className="w-full img lg:basis-6/12 mb-10 lg:mb-0 ">
                     <div className="flex flex-col w-full relative">
-                    <img className='w-full px-6 mx-auto hover:bg-transparent' src={imgs[0].url} alt={imgs[0].filename} />
+                    <img className='w-full px-6 mx-auto hover:bg-transparent' src={mailImg.url} alt={mailImg.filename} />
                     <div className="flex mt-4 justify-between px-6 gap-2">
                        {
                         imgs?.map((img,index)=>{
-                            return <div key={index} className=" md:h-24">
-                                <img className='h-full' src={img.url} alt={img.filename} />
+                            return <div key={index} className="h-16 md:h-24">
+                                <img className='h-full' onClick={()=>setMainImg(img)} src={img.url} alt={img.filename} />
                             </div>
                         })
                        }
