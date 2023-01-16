85c0fc917d888e87217eeff720ee48d2bc46b550
anishazahan <anishazahan13@gmail.com>
2023-01-16 10:56:16 +0600

    color btn design

--- src/Pages/Products2/AddToCart.js
+++ src/Pages/Products2/AddToCart.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
-const AddToCart = ({}) => {
+const AddToCart = ({product}) => {
+    const {id,stock,colors} = product;
+
   return (
-    <div>AddToCart</div>
+    <div>
+        <p className='flex space-x-2 items-center'>
+            colors : 
+            {
+                colors.map  ((color,index)=>{
+                        return <button style={{backgroundColor:color}} className='text-white w-4 h-4 ml-3 rounded-[50%] '  key={index}>
+                            
+                        </button>
+                })
+            }
+        </p>
+
+    </div>
   )
 }
 
