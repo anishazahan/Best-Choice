51565b45fcc646c3368ef3a47660941c0c3e0fb4
anishazahan <anishazahan13@gmail.com>
2023-01-18 07:55:10 +0600

    color picker selection functionality

--- src/Pages/Products2/AddToCart.js
+++ src/Pages/Products2/AddToCart.js
@@ -1,17 +1,29 @@
 import React from 'react'
+import { useState } from 'react';
+import { BsCheck } from "react-icons/bs";
 
 const AddToCart = ({product}) => {
     const {id,stock,colors} = product;
+    const [color,setColor] = useState(colors[0]);
 
   return (
     <div>
+
+        {/* ///......color btn,,,,,,,,, */}
+
         <p className='flex space-x-2 items-center'>
             colors : 
             {
-                colors.map  ((color,index)=>{
-                        return <button style={{backgroundColor:color}} className='text-white w-4 h-4 ml-3 rounded-[50%] '  key={index}>
-                            
+                colors.map  ((colorEle,index)=>{
+                        return <button 
+                        style={{backgroundColor:colorEle}}
+                         className={` ${color === colorEle ? "w-4 h-4 rounded-[50%] ml-4 opacity-100" : "w-3 h-3 rounded-[50%] ml-4 opacity-30 hover:opacity-100"}`}  
+                         onClick={() => setColor(colorEle)}
+                         key={index}
+                         > 
+                        { color === colorEle ? <BsCheck className='text-white font-bold'/> : null }
                         </button>
+                     
                 })
             }
         </p>
