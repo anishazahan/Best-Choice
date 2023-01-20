8774d7028845a0916a99417edd559a86fbf8b516
anishazahan <anishazahan13@gmail.com>
2023-01-20 18:30:06 +0600

    search field design

--- src/Pages/Products2/FilterProducts.js
+++ src/Pages/Products2/FilterProducts.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const FilterProducts = () => {
   return (
-    <div>FilterProducts</div>
+    <div>
+
+      <div className="flex flex-col">
+        <form >
+          <input type="text" placeholder='search'  className='placeholder-black border-2 p-2 font-medium focus:outline-secondary'/>
+        </form>
+      </div>
+    </div>
   )
 }
 
