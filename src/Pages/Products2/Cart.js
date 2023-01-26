923ff1288678f723f9b5e07d19971d10901e79be
anishazahan <anishazahan13@gmail.com>
2023-01-26 08:18:49 +0600

    cart list heading design

--- src/Pages/Products2/Cart.js
+++ src/Pages/Products2/Cart.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useCartContext } from '../../Hooks/cart_context'
 
 const Cart = () => {
+
+const {cart} = useCartContext();
+
+
+
   return (
-    <div>Cart</div>
+    <div className=' px-5 w-full md:px-10 lg:px-0 lg:max-w-4xl mx-auto my-20'>
+        <div className="grid grid-cols-5 text-center border-b-2 py-2">
+            <h2>Item</h2>
+            <h2 className='hidden md:block'>price</h2>
+            <h2>Quantity</h2>
+            <h2 className='hidden md:block'>Subtotal</ h2>
+            <h2>Remove</h2>
+            
+        </div>
+    </div>
   )
 }
 
