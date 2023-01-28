d75947c253167244c2a825957d6df7183f140a88
anishazahan <anishazahan13@gmail.com>
2023-01-28 19:08:52 +0600

    update reducer file

--- src/Hooks/reducer/cartReducer.js
+++ src/Hooks/reducer/cartReducer.js
@@ -114,7 +114,7 @@ const cartReducer = (state, action) => {
         
 
         if (action.type === "CART_ITEM_PRICE_TOTAL") {
-            let { total_item, total_price } = state.cart.reduce(
+            let { total_item, total_price } = state?.cart?.reduce(
               (accum, curElem) => {
                 let { price, amount } = curElem;
         
